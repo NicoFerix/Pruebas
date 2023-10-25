15d3cbaabfb24646d03abdc1d735032a2a905c6f
NicoFerix <nicolas_ferix@hotmail.com>
2023-10-25 11:49:25 -0300

    Backup de factura_articulos como figura en el master

--- Ejercicios por bandera/factura_articulos.py
+++ Ejercicios por bandera/factura_articulos.py
@@ -3,4 +3,27 @@
 # ingresando de la forma “¿Desea ingresar otro artículo? [S/N]”. La carga de datos 
 # finaliza cuando se detecta una n o N. La computadora debe mostrar el monto de la factura
 
-# Que odna papá
+precioUnitario = 0
+cantArticulos = 0
+nvoArticulo = ""
+factura = 0
+continuar = True
+
+while continuar:
+
+    nvoArticulo = input("¿Desea ingresar un artículo? [S/N] : ")
+
+    if nvoArticulo.lower() == "s":
+        cantArticulos = int(input("Ingrese la cantidad de artículos que desea comprar : "))
+        precioUnitario = int(input("Ingrese el precio unitario del artículo : "))
+        factura += cantArticulos * precioUnitario
+
+    elif nvoArticulo.lower() == "n":
+        continuar = False
+        
+    else:
+        print("ERROR - Opción incorrecta. Intente nuevamente")
+
+print(f"El monto total de la factura es : ${factura}")
+print("Adiós...")
+
